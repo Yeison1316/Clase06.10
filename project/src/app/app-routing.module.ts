1e77966f62e5db937c3902a69bb2662fe546a736
Yeison Mestra <ymmestram@ufpso.edu.co>
2023-11-03 23:27:50 -0500

    Guard

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+import { ProductosModule } from './productos/productos.module';
 
 const routes: Routes = [
   {path: '' , loadChildren : ()=>{return import("./home/home.module").then(m => m.HomeModule)}},
-  {path: 'producto' , loadChildren : ()=>{return import("./productos/productos.module").then(p => p.ProductosModule)}}
+  { path : 'producto' , canActivate: [authGuard],  loadChildren : ()=>{return import("./productos/productos.module").then(m => m.ProductosModule)}},
 ];
 
 @NgModule({
